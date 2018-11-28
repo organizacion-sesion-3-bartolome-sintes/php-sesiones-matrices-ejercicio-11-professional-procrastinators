adf2daf838a3e2306dbe9556c6361c8812a77955
Bartolomé Sintes <bartolome.sintes@gmail.com>
2018-11-28 22:52:17 +0100

    Resuelvo ejercicio

--- cs-matrices-1-11/matrices-1-11-1.php
+++ cs-matrices-1-11/matrices-1-11-1.php
@@ -2,12 +2,38 @@
 /**
  * Elimine dibujos - matrices-1-11-1.php
  *
- * @author    Escriba su nombre
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2018 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2018-10-31
+ * @link      http://www.mclibre.org
  *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-print "<!-- Ejercicio incompleto -->\n";
+session_name("matrices-1-11");
+session_start();
 
+$caracterMinimo = 128000;
+$caracterMaximo = 128060;
+$_SESSION["numeroDibujos"] = 7;
+
+if (!isset($_SESSION["dibujos"]) || count($_SESSION["dibujos"]) == 0) {
+    for ($i = 0; $i < $_SESSION["numeroDibujos"]; $i++) {
+        $_SESSION["dibujos"][$i] = mt_rand($caracterMinimo, $caracterMaximo);
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,7 +42,7 @@ print "<!-- Ejercicio incompleto -->\n";
   <title>
     Elimine dibujos.
     foreach (1). Sesiones.
-    Escriba su nombre
+    Ejercicios. PHP. Bartolomé Sintes Marco. www.mclibre.org
   </title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color" />
@@ -27,13 +53,33 @@ print "<!-- Ejercicio incompleto -->\n";
 
 <body>
 <?php
-
-print "/* Ejercicio incompleto */\n";
-
+print "  <h1>$_SESSION[numeroDibujos] dibujos, y quedaron " . count($_SESSION["dibujos"]) . "</h1>\n";
+print "\n";
+print "  <p>Haga clic en un dibujo para eliminarlo.</p>\n";
+print "\n";
+print "  <form action=\"matrices-1-11-2.php\">\n";
+print "    <p>\n";
+foreach ($_SESSION["dibujos"] as $indice => $valor) {
+    print "      <button name=\"elimina\" value=\"$indice\" style=\"font-size: 400%\">\n";
+    print "        &#$valor;\n";
+    print "      </button>\n";
+}
+print "    </p>\n";
+print "  </form>\n";
 ?>
 
   <footer>
-    <p>Escriba su nombre</p>
+    <p class="ultmod">
+      Última modificación de esta página:
+      <time datetime="2018-10-31">31 de octubre de 2018</time>
+    </p>
+
+    <p class="licencia">
+      Este programa forma parte del curso <strong><a href="http://www.mclibre.org/consultar/php/">Programación
+      web en PHP</a></strong> de <a href="http://www.mclibre.org/" rel="author" >Bartolomé Sintes Marco</a>.<br />
+      El programa PHP que genera esta página se distribuye bajo
+      <a rel="license" href="http://www.gnu.org/licenses/agpl.txt">licencia AGPL 3 o posterior</a>.
+    </p>
   </footer>
 </body>
 </html>
